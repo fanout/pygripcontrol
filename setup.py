85f1b6d95becb54bf9b1770f7bc63512dc6aa703
Justin Karneges <justin@affinix.com>
2013-04-05 01:20:59 -0700

    rename and make packagable for pypi

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+name="gripcontrol",
+version="1.0.0",
+description="GRIP library",
+author="Justin Karneges",
+author_email="justin@affinix.com",
+url="https://github.com/fanout/pygripcontrol",
+license="MIT",
+py_modules=["gripcontrol"],
+install_requires=["pubcontrol==1.0.2"],
+classifiers=[
+	"Topic :: Utilities",
+	"License :: OSI Approved :: MIT License"
+]
+)
