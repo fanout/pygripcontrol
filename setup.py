e730950a260dc98ba880b323739d3c7ed0ac52b9
Justin Karneges <justin@affinix.com>
2013-10-11 01:02:20 -0700

    correct pubcontrol dependency

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ from setuptools import setup
 
 setup(
 name="gripcontrol",
-version="1.0.2",
+version="1.0.3",
 description="GRIP library",
 author="Justin Karneges",
 author_email="justin@affinix.com",
 url="https://github.com/fanout/pygripcontrol",
 license="MIT",
 py_modules=["gripcontrol"],
-install_requires=["pubcontrol==1.0.2"],
+install_requires=["pubcontrol>=1.0.2"],
 classifiers=[
 	"Topic :: Utilities",
 	"License :: OSI Approved :: MIT License"
