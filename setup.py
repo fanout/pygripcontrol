b753dd4c7ffc416edb914f8b82ff318acda8442c
Justin Karneges <justin@affinix.com>
2015-01-08 11:13:15 -0800

    bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
 name="gripcontrol",
-version="2.0.0",
+version="2.1.0",
 description="GRIP library",
 author="Justin Karneges",
 author_email="justin@affinix.com",
