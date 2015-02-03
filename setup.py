a799f42599e5ab9b8fe11e95bfa06c9d780fd0bd
Kon <bokarius@comcast.net>
2015-02-02 16:06:40 -0800

    updated dependency versions

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ from setuptools import setup
 
 setup(
 name="gripcontrol",
-version="2.2.0",
+version="2.2.1",
 description="GRIP library",
 author="Justin Karneges",
 author_email="justin@affinix.com",
 url="https://github.com/fanout/pygripcontrol",
 license="MIT",
 py_modules=["gripcontrol"],
-install_requires=["PyJWT>=0.1.6", "pubcontrol>=2.0.0"],
+install_requires=["PyJWT>=0,<1", "pubcontrol>=2,<3"],
 classifiers=[
 	"Topic :: Utilities",
 	"License :: OSI Approved :: MIT License"
