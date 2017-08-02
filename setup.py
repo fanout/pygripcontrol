3bb63ea1d0d258ae8d8badace430ed1c636832ab
Justin Karneges <justin@karneges.com>
2017-08-02 16:28:59 -0700

    update version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
 name="gripcontrol",
-version="2.4.2",
+version="2.5.0",
 description="GRIP library",
 author="Justin Karneges",
 author_email="justin@affinix.com",
@@ -12,7 +12,7 @@ url="https://github.com/fanout/pygripcontrol",
 license="MIT",
 package_dir={'gripcontrol': 'src'},
 packages=['gripcontrol'],
-install_requires=["PyJWT>=1,<2", "pubcontrol>=2.2.4,<3"],
+install_requires=["PyJWT>=1,<2", "pubcontrol>=2.4.1,<3"],
 classifiers=[
 	"Topic :: Utilities",
 	"License :: OSI Approved :: MIT License"
