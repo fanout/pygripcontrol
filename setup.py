ed81d8808a1d6eaa2841a0d0eee76db82629ad8d
Justin Karneges <justin@karneges.com>
2019-04-18 19:34:16 -0700

    update version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
 name='gripcontrol',
-version='3.3.0',
+version='3.3.1',
 description='GRIP library',
 author='Justin Karneges',
 author_email='justin@fanout.io',
