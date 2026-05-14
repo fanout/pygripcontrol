70a84cbc19a2e7cc2c664a88b829fe1209bac6f7
Justin Karneges <jkarneges@fastly.com>
2026-05-14 14:08:56 -0700

    update version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="gripcontrol",
-    version="4.2.1",
+    version="4.3.0",
     description="GRIP library",
     author="Justin Karneges",
     author_email="justin@fanout.io",
